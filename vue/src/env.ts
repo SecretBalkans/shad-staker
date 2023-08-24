9e3e8d29f093e1a20f1ef2115a7e95b73b2a39c8
hadzija7 <hadzijasale@gmail.com>
2023-08-24 20:19:10 +0200

    env changed for connection to cosmoshub mainnet

--- vue/src/env.ts
+++ vue/src/env.ts
@@ -1,5 +1,5 @@
-const apiURL = import.meta.env.VITE_API_COSMOS ?? "http://localhost:1317";
-const rpcURL = import.meta.env.VITE_WS_TENDERMINT ?? "http://localhost:26657";
+const apiURL = import.meta.env.VITE_API_COSMOS ?? "https://rest.cosmos.directory/cosmoshub" //"http://localhost:1317";
+const rpcURL = import.meta.env.VITE_WS_TENDERMINT ?? "https://rpc.cosmos.directory/cosmoshub"//"http://localhost:26657";
 const prefix = import.meta.env.VITE_ADDRESS_PREFIX ?? "cosmos";
 
 export const env = {
